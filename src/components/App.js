44d53288d39bcdaced288a56ed5dc76cb5e6eb04
Saloni <salonimehta2127@gmail.com>
2021-07-26 01:55:37 -0400

    completed first 3 deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushiList, setSushiList] = useState([]);
+  const [index, setIndex] = useState(0)
+  const [newSushiList, setNewSushiList] = useState(sushiList)
+
+  useEffect(() => {
+    fetch(API).then(resp => resp.json()).then(data => {
+      setSushiList(data)
+      setNewSushiList(data.slice(index, index + 4))
+    })
+  }, [index])
+
+  function handleClickMore() {
+    setIndex(() => index + 4)
+    setNewSushiList(sushiList.slice(index, index + 4))
+  }
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushiList={sushiList} newSushiList={newSushiList} onMoreClick={handleClickMore} />
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,18 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({ sushiList, newSushiList, onMoreClick }) {
+
+  const sushis = newSushiList.map(sushi => <Sushi key={sushi.id}
+    name={sushi.name}
+    image={sushi.img_url}
+    price={sushi.price}></Sushi>)
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushis}
+      <MoreButton sushiList={sushiList} onMoreClick={onMoreClick} />
     </div>
   );
 }
